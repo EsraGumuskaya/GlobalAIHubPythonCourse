b2ba70d8076699ce25bab5c2869b134b5afa7120
EsraGumuskaya <78369180+EsraGumuskaya@users.noreply.github.com>
2021-02-01 22:27:22 +0100

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+Val1=int(input("1. Val:"))
+Val2=int(input("2. Val:"))
+Val3=int(input("3. Val:"))
+Val4=int(input("4. Val:"))
+Val5=int(input("5. Val:"))
+print("1.Val:",Val1,",","2.Val:",Val2,",", "3.Val:",Val3,",", "4.Val:",Val4, ",", "5.Val:",Val5)
+
+
+# In[13]:
+
+
+print(type(Val1))
+print(type(Val2))
+print(type(Val3))
+print(type(Val4))
+print(type(Val5))
+
+
+# In[ ]:
+
+
+
+
